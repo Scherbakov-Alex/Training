2e8d6b5f16e3809883de7ca833c4c0bccdac9f30
Alex Scherbakov <skinerik@gmail.com>
2021-04-15 11:42:17 +0300

    0.1

---
+++ Money.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ConsoleConverter
+{
+    public class Money
+    {
+        decimal moneyValue;
+        decimal moneyCounts;
+        public string m_currency;
+        public decimal finalValue;
+        Dictionary<string, int> valut = new Dictionary<string, int>
+        {
+            ["RUB"] = 1,
+            ["USD"] = 76,
+            ["EUR"] = 89,
+        };
+        public Money()
+        {
+        }
+
+        public Money(decimal value, string currency)
+        {
+            moneyValue = value;
+            moneyCounts = valut[currency];
+            m_currency = currency;
+            finalValue = moneyValue * moneyCounts;
+        }
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            Money mn = new Money
+            {
+                finalValue = Math.Round((m1.finalValue + m2.finalValue) / m1.moneyCounts, 2),
+                m_currency = m1.m_currency
+            };
+            return mn;
+        }
+        public static Money operator -(Money m1, Money m2)
+        {
+            Money mn = new Money
+            {
+                finalValue = Math.Round((m1.finalValue - m2.finalValue) / m1.moneyCounts, 2),
+                m_currency = m1.m_currency
+            };
+            return mn;
+        }
+        public static Money operator *(Money m1, Money m2)
+        {
+            Money mn = new Money
+            {
+                finalValue = Math.Round((m1.finalValue * m2.finalValue) / (m1.moneyCounts * m2.moneyCounts), 6),
+                m_currency = m1.m_currency
+            };
+            return mn;
+        }
+        public static Money operator /(Money m1, Money m2)
+        {
+            Money mn = new Money
+            {
+                finalValue = Math.Round((m1.finalValue / m2.finalValue) / (m1.moneyCounts * m2.moneyCounts), 2),
+                m_currency = m1.m_currency
+            };
+            return mn;
+        }
+        public static bool operator <(Money m1, Money m2)
+        {
+            if (m1.finalValue < m2.finalValue)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator >(Money m1, Money m2)
+        {
+            if (m1.finalValue > m2.finalValue)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator ==(Money m1, Money m2)
+        {
+            if (m1.finalValue == m2.finalValue)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator !=(Money m1, Money m2)
+        {
+            if (m1.finalValue != m2.finalValue)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator >=(Money m1, Money m2)
+        {
+            if (m1.finalValue >= m2.finalValue)
+                return true;
+            else
+                return false;
+        }
+        public static bool operator <=(Money m1, Money m2)
+        {
+            if (m1.finalValue <= m2.finalValue)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+
+namespace ConsoleConverter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите выражение которое необходимо сравнить в формате\n 1 RUB > 2 EUR");
+            string str = Console.ReadLine();
+            string[] words = str.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                Money a = new Money(Convert.ToDecimal(words[0]), words[1]);
+                Money b = new Money(Convert.ToDecimal(words[3]), words[4]);
+                string operation = words[2];
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine($"{(a + b).finalValue} {a.m_currency}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"{(a - b).finalValue} {a.m_currency}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"{(a * b).finalValue} {a.m_currency}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"{(a / b).finalValue} {a.m_currency}");
+                        break;
+                    case ">":
+                        Console.WriteLine($"{a > b}");
+                        break;
+                    case "<":
+                        Console.WriteLine($"{a < b}");
+                        break;
+                    case "=":
+                        Console.WriteLine($"{a == b}");
+                        break;
+                    case ">=":
+                        Console.WriteLine($"{a >= b}");
+                        break;
+                    case "<=":
+                        Console.WriteLine($"{a <= b}");
+                        break;
+                    default:
+                        Console.WriteLine("Вам доступны операции: +-*/ и ><= >=<=");
+                        break;
+
+                }
+                Console.ReadLine();
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Выражение необходимо вводить по маске: 1 EUR + 5 USD");
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+   
+   
+}
